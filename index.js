51fd11aa5fefb5143d9facc3a53f2d9c81164f94
Nathan <LilFuze9@gmail.com>
2023-12-02 10:40:53 -0500

    stuff

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add JavaScript below
+var weekNum = 1; 
+var startDate = new Date(); //Default is system's date
+var currDate = new Date(startDate);
+
+function addRow() {
+    var table = document.getElementById("main-table");
+    var row = table.insertRow(table.rows.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    currDate.setDate(startDate.getDate() + (7 * weekNum));
+    currDate.setDate(currDate.getDate() - (currDate.getDay() - startDate.getDay()));  //the code to increment the date is weird
+    currDate.setMonth(currDate.getMonth() - 1);
+
+    cell1.innerHTML = "Week " + String(++weekNum);
+    cell2.innerHTML = currDate.toDateString();
+    cell3.innerHTML = "hello";
+}
